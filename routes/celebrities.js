9f911310395f1193ba26c3889ad55e6d60f2f077
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-06 14:48:50 +0200

    add post route to delete celebrities

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -12,6 +12,11 @@ router.get('/celebrities', (req, res, next) => {
 		.catch(err => next(err))
 })
 
+// celebrities/new view
+router.get('/celebrities/new', (req, res, next) => {
+	res.render('celebrities/new')
+});
+
 // the celebrity details page
 router.get('/celebrities/:id', (req, res, next) => {
 	const celebrityId = req.params.id
@@ -22,5 +27,31 @@ router.get('/celebrities/:id', (req, res, next) => {
 		})
 		.catch(err => next(err))
 });
+
+// post route for adding new celebrities
+router.post('/celebrities', (req, res, next) => {
+	// console.log(req.body)
+	const { name, occupation, catchPhrase } = req.body
+	Celebrity.create({ name, occupation, catchPhrase })
+		.then(createdCelebrity => {
+			console.log(createdCelebrity)
+			// redirect to '/celebrities/<id of the celebrity>
+			res.redirect(`/celebrities/${createdCelebrity._id}`)
+			// we could also render the view again and pass
+			// the object of the created book
+			// res.render('celebrities/show', { celebrity: createdCelebrity })
+		})
+		.catch(err => next(err))
+
+});
+
+// post route to delete a celebrity from the database
+router.post('/celebrities/:celebrityId/delete', (req, res, next) => {
+	const { celebrityId } = req.params;
+
+	Celebrity.findByIdAndDelete(celebrityId)
+		.then(() => res.redirect('/celebrities'))
+		.catch(err => next(err))
+})
 
 module.exports = router;
